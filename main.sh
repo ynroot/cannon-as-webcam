c3ab72bc525250be04b402558f6d86b9516c96f8
Ynroot Lab <ynrootlab@pop-os.localdomain>
2021-11-17 20:44:13 +0000

    using only the main script to exec all the commands

--- main.sh
+++ main.sh
@@ -3,7 +3,7 @@
 
 rc=1 # OK button return code =0 , all others =1
 while [ $rc -eq 1 ]; do
-  ans=$(zenity --info --title 'YNROOT TEST' --window-icon='' \
+  ans=$(zenity --info --title 'YNROOT TEST' --window-icon='/images/logo.png' \
        --text 'Webcam CTRL' \
       --extra-button START \
       --extra-button STOP \
@@ -16,10 +16,20 @@ while [ $rc -eq 1 ]; do
   if [[ $ans = "START" ]]
   then
         echo "Starting connection to the camera!"
-        gnome-terminal -- './connect_link.sh'
+        sudo modprobe v4l2loopback
+
+        gphoto2 --auto-detect
+        echo "_____________________________________________________________________"
+
+        gphoto2 --abilities
+        echo "_____________________________________________________________________"
+        sleep 2
+
+        gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video2
+
   elif [[ $ans = "STOP" ]]
   then
         echo "cmd 3"
-        gnome-terminal -- './stop_link.sh'
+        killall gphoto2 gvfs-gphoto2-volume-monitor
   fi
 done
